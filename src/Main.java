764f607d5605bf763e6edfd6d72ea288118555df
Bharath chandra chowdary <bharathpc01@gmail.com>
2025-09-05 08:30:19 +0530

    wrote first variable

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,16 @@
 public class Main {
 
     public static void main(String[] args){
+        /*
         System.out.print("Hello World!\n");
         System.out.print("its working! damn");
         System.out.print("\ni like pizzaaaaaaa!");
-        // this is a comment and i think its my first time writing comment in java
+        // this is a comment and i think it's my first time writing comment in java
+        System.out.println("oh ho");
 
+         */
+        int age = 19;
+        System.out.println(age);
 
 
     }
